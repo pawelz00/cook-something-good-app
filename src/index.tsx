bdd704a77e56163c815eaa3da7c7083bf11f12c6
pawelz00 <pawet.wets@gmail.com>
2023-05-08 19:04:35 +0200

    added react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './styles/index.css';
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );

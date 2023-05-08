e03b0d0e6e70c10712674191017bcc093b98a57d
pawelz00 <pawet.wets@gmail.com>
2023-05-08 18:13:04 +0200

    picocss added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="text-3xl font-bold underline">
-        Hello world!
-        </h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <nav className="container">
+      <ul>
+        <li><strong>CookSomethingGood</strong></li>
+        <li><a href="#">Przepisy A-Z</a></li>
+        <li><a href="#">Kategorie</a></li>
+        <li><a href="#">Losowy przepis</a></li>
+      </ul>
+    </nav>
   );
 }
 

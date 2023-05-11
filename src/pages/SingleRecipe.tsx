55b919609f9fbd751b839fb557190400b3b1918b
pawelz00 <pawet.wets@gmail.com>
2023-05-12 00:02:47 +0200

    SingleRecipe created

---
+++ src/pages/SingleRecipe.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import { ListOfMeals} from '../types/Meals';
+import ApiService from '../services/ApiService';
+import Recipe from '../components/Recipe';
+import { useParams } from 'react-router-dom';
+
+const SingleRecipe: React.FC = () => {
+
+  const params = useParams();
+
+  const [recipe, setRecipe] = useState<ListOfMeals>();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSingleRecipe();
+  }, []);
+
+  const getSingleRecipe = async () => {
+    await ApiService.getSingleMeal(params.id)
+      .then((response: any) => {
+        setRecipe(response.data);
+        setIsLoading(false);
+      })
+      .catch((e: Error) => {
+        console.log(e);
+      });
+  };
+
+  return(
+      isLoading ? (
+        <main className='container'>
+          <article>
+            <h1>Loading...</h1>
+          </article>
+        </main>
+      ) : (
+        <Recipe recipe={recipe?.meals[0]}/>
+      )
+  );
+}
+
+export default SingleRecipe;
